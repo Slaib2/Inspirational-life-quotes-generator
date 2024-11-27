20930b74d535e56f6c0dd0527e2bac43c377d31c
Slaib2 <slaibhatasara@gmail.com>
2024-11-28 01:51:53 +0200

    Added the basic form handling and typewriter

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function generateQuote(event) {
+  event.preventDefault();
+}
+let quoteElement = document.querySelector("#quote");
+
+new Typewriter("#quote", {
+  strings: ["You have to believe in yourself when no one else does."],
+  autoStart: true,
+  delay: 2,
+  cursor: "",
+});
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
