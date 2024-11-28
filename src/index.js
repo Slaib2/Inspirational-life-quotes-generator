2f09da841d8c84e564b5b977f79cb188b17ded15
Slaib2 <slaibhatasara@gmail.com>
2024-11-28 03:04:45 +0200

    Integrated API onto the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-function generateQuote(event) {
-  event.preventDefault();
-  let quoteElement = document.querySelector("#quote");
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: "You have to believe in yourself when no one else does.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 2,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "66be36afb0bfdbdtc4e9dd41a4603o8c";
+  let prompt = `User instrctions:Generate inspirational life quotes about ${instructionsInput.value};`;
+  let context =
+    "You are an inspirational assistant.You like to inspire others.Your task is to generate a short life quote using.Do not include the author of the quote. Sign off the quote with 'SheCodes AI' and put a related emoji at the end.Please follow the user instructions";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiURL).then(displayQuote);
+  let quoteElement = document.querySelector("#quote");
 }
 let quoteFormElement = document.querySelector("#quote-generator-form");
 quoteFormElement.addEventListener("submit", generateQuote);
