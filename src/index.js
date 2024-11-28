0261d6610e2cf69d88480aa596a8b48a8a0798a9
Slaib2 <slaibhatasara@gmail.com>
2024-11-28 03:54:35 +0200

    Improved the user experience of the project

--- src/index.js
+++ src/index.js
@@ -15,8 +15,11 @@ function generateQuote(event) {
   let context =
     "You are an inspirational assistant.You like to inspire others.Your task is to generate a short life quote using.Do not include the author of the quote. Sign off the quote with 'SheCodes AI' and put a related emoji at the end.Please follow the user instructions";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  axios.get(apiURL).then(displayQuote);
+
   let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating">⏳ Generating a life quote about ${instructionsInput.value}</div>`;
+  axios.get(apiURL).then(displayQuote);
 }
 let quoteFormElement = document.querySelector("#quote-generator-form");
 quoteFormElement.addEventListener("submit", generateQuote);
